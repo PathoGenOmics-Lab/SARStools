0561e3641c2aa6b7c331d7b7b63ac8da039fbafd
Paula Ruiz-Rodriguez <50167687+Paururo@users.noreply.github.com>
2024-06-29 11:02:47 +0200

    add retrieve sars sequence and arguments

--- get_mutations.py
+++ get_mutations.py
@@ -2,13 +2,25 @@
 import argparse
 import os
 import pandas as pd
+import requests
 
-
-
-
+def getsars_ref():
+    url = "https://www.ebi.ac.uk/ena/browser/api/fasta/MN908947.3"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        sars_ref_content = response.text
+        return sars_ref_content
+    else:
+        print(f"Error in the EBI server: {response.status_code} {response.reason}")
+        return None
 
 def main():
+    parser = argparse.ArgumentParser(description='Get mutations from SARS-CoV-2 sequences')
+    #parser.add_argument('-i', '--input', help='', required=True)
+    #parser.add_argument('-o', '--output', help='', required=True)
+    args = parser.parse_args()
 
-
+    ref_seq = "".join(getsars_ref().split('\n')[1:])
 if __name__ == '__main__':
     main()
